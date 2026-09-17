5f1a2c7294071d41e9e0946a64090ba0e4cb8898
sunil-local <sunilkrdeep@yahoo.com>
2026-09-17 21:54:22 +0530

    Fix Terraform formatting

--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "subscription_id" {
   type        = string
 
   validation {
-    condition     = can(regex(
+    condition = can(regex(
       "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$",
       var.subscription_id
     ))
