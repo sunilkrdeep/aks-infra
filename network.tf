a2c97368a09738fcc9378c3f93f50c1d88fe4302
sunil-local <sunilkrdeep@yahoo.com>
2026-09-18 17:30:09 +0530

    fix: add import blocks for vnet and subnet

--- network.tf
+++ network.tf
@@ -15,6 +15,17 @@ import {
   to = azurerm_resource_group.aks
   id = "/subscriptions/3a5237f2-fd9c-490b-a303-8bd660d58441/resourceGroups/aks-sutramind-rg"
 }
+
+import {
+  to = azurerm_virtual_network.aks
+  id = "/subscriptions/3a5237f2-fd9c-490b-a303-8bd660d58441/resourceGroups/aks-sutramind-rg/providers/Microsoft.Network/virtualNetworks/aks-sutramind-vnet"
+}
+
+import {
+  to = azurerm_subnet.aks
+  id = "/subscriptions/3a5237f2-fd9c-490b-a303-8bd660d58441/resourceGroups/aks-sutramind-rg/providers/Microsoft.Network/virtualNetworks/aks-sutramind-vnet/subnets/snet-aks"
+}
+
 resource "azurerm_resource_group" "aks" {
   name     = var.resource_group_name
   location = var.location
